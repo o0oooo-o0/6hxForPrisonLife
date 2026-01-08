b2cb49aa694a3637e40447472b82e7179594e7cc
o0oooo-o0 <jznizr@gmail.com>
2026-01-08 14:30:55 -0500

    Add 6hxLoader.lua for script loading
    Implement universal loader for game scripts

---
+++ 6hxLoader.lua
@@ -0,0 +1,29 @@
+
+--[[
+
+
+
+                                       
+  ad8888ba,  88                        
+ 8P'    "Y8  88                        
+d8           88                        
+88,dd888bb,  88,dPPYba,   8b,     ,d8  
+88P'    `8b  88P'    "8a   `Y8, ,8P'   
+88       d8  88       88     )888(     
+88a     a8P  88       88   ,d8" "8b,   
+ "Y88888P"   88       88  8P'     `Y8  Universal Loader
+                                       
+                                       
+
+
+
+]]
+
+if getgenv().sixhx then return end
+getgenv().sixhx = true
+
+loadstring(game:HttpGet(
+    game.PlaceId == 155615604 and "https://pandadevelopment.net/virtual/file/b265cebe288cca07"
+    or game.PlaceId == 0 and "nil"
+    or "https://raw.githubusercontent.com/o0oooo-o0/6hxForPrisonLife/refs/heads/master/unsupported.lua" -- kick if unsupported
+))()
